4bc360c7e6582218f0482181ca2de6b41dc390db
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-17 02:47:09 +0300

    Update myFirstApp1.py

--- myFirstApp1.py
+++ myFirstApp1.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib as mtp
-import plotly as plt
 st.write('Hello')
 df=pd.read_excel(r'C:\Users\engsa\OneDrive\الأرشيف\2023\00\PowerBI\Projects\Collection_2\Collection_All.xlsx')
 st.write(df)
