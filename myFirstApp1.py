05ea255630f3df7d692985de5d6120f725361e10
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-17 02:10:30 +0300

    Update myFirstApp1.py

--- myFirstApp1.py
+++ myFirstApp1.py
@@ -5,3 +5,4 @@ import plotly as plt
 st.write('Hello')
 df=pd.read_excel(r'C:\Users\engsa\OneDrive\الأرشيف\2023\00\PowerBI\Projects\Collection_2\Collection_All.xlsx')
 st.write(df)
+st.write('finish')
