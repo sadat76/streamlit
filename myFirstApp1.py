97465910e4571c2e65b673922bf9843f585ff031
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-18 01:14:11 +0300

    Update myFirstApp1.py

--- myFirstApp1.py
+++ myFirstApp1.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.write('Hello')
 ## Data
- data = pd.read_excel('Collection_All.xlsx',sheet_name = 'Sheet1')
+ data = pd.read_excel('streamlit1.xlsx',sheet_name = 'Sheet1')
   st.write(data)
 st.write('finish')
