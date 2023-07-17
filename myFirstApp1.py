0b65a30f5e65ab214d2074c2d34955f762671577
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-17 03:44:17 +0300

    Update myFirstApp1.py

--- myFirstApp1.py
+++ myFirstApp1.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.write('Hello')
   path = "Collection_All.xlsx"
     if not os.path.isfile(path):
-        path = f"https://github.com/sadat76/streamlit/main/{path}"
+        path = f"https://github.com/sadat76/streamlit/war/main/{path}"
     data = pd.read_excel(path)
 #df=pd.read_excel(r'C:\Users\engsa\OneDrive\الأرشيف\2023\00\PowerBI\Projects\Collection_2\Collection_All.xlsx')
 st.write(data)
