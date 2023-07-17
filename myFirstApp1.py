3a97992c4c89d7608ec3fc57f702ad6c7e5667ea
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-17 03:08:30 +0300

    Update myFirstApp1.py

--- myFirstApp1.py
+++ myFirstApp1.py
@@ -1,6 +1,11 @@
+import os
 import streamlit as st
 import pandas as pd
 st.write('Hello')
-df=pd.read_excel(r'C:\Users\engsa\OneDrive\الأرشيف\2023\00\PowerBI\Projects\Collection_2\Collection_All.xlsx')
+  path = "Collection_All.xlsx"
+    if not os.path.isfile(path):
+        path = f"https://github.com/sadat76/streamlit/main/{path}"
+    data = pd.read_excel(path)
+#df=pd.read_excel(r'C:\Users\engsa\OneDrive\الأرشيف\2023\00\PowerBI\Projects\Collection_2\Collection_All.xlsx')
 st.write(df)
 st.write('finish')
