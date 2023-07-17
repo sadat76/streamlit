47f9259dff7514d811f33c2d4c70cb2d0149642c
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-18 02:01:29 +0300

    Update stream1.py

--- stream1.py
+++ stream1.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.write('Hello')
-
+import pandas as pd
+import numpy as np
+df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])st.line_chart(df)
 st.write('finish')
