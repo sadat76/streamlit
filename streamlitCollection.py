f303815b3d1fb7698a91a6c1d899d7d2e3549ed2
sadat76 <35308049+sadat76@users.noreply.github.com>
2023-07-23 16:32:32 +0300

    add files

--- streamlitCollection.py
+++ streamlitCollection.py
@@ -7,29 +7,24 @@ import plotly.express as px
 
 st.title('Collection Dashboard')
 
-DATE_COLUMN = 'freeze_dt'
 DATA_URL = (r'Collection2023_streamlit.csv')
-
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    data=data.groupby(['main_branch','customer_class','month'])['amount'].agg(['sum'])
-    return data
-
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
-
-st.subheader('Collection By Month')
-df= pd.read_csv(DATA_URL)
-df=df.groupby(['Main_Branch','CUSTOMER_CLASS','month'])['Amount'].agg(['sum'])
-fig=plt.figure(figsize=(15,8))
-plt.plot(df['main_branch'],df['amount'])
+data = pd.read_csv(DATA_URL)
+lowercase = lambda x: str(x).lower()
+data.rename(lowercase, axis='columns', inplace=True)
+df =data.groupby(['branch_name','main_branch','customer_class','month'])['amount'].agg(['sum'])
+df=df.reset_index()
+df2=df['customer_class']
+st.dataframe(df2)
+fig = plt.figure(figsize=(15,8))
+plt.plot(df['main_branch'],df['sum'])
 st.pyplot(fig)
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(data)
+    st.write(data['branch_name'])
+st.subheader('Collection By Month')
+# df=pd.read_csv(DATA_URL)
+# dfGroup=df.groupby(['Main_Branch','CUSTOMER_CLASS','month'])['Amount'].agg(['sum'])
+# st.write(dfGroup)
+
+
